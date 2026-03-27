5175293131432657fd1d178f2527bc255e4bfae2
TomekG11 <tomekgregori21@gmail.com>
2026-03-27 17:27:03 +0100

    Update UkrainskiReaderMod.java

--- src/main/java/com/example/UkrainskiReaderMod.java
+++ src/main/java/com/example/UkrainskiReaderMod.java
@@ -9,19 +9,19 @@ import net.minecraft.client.util.InputUtil;
 public class UkrainskiReaderMod implements ClientModInitializer {
     public static KeyBinding openGuiKey;
 
+    @Override
     public void onInitializeClient() {
         BlacklistTracker.init();
         openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "key.ukrainskireader.open",
             InputUtil.Type.KEYSYM,
-            71, // G key
+            71,
             "category.ukrainskireader"
         ));
-        
+
         ClientTickEvents.END_CLIENT_TICK.register((client) -> {
             PlayerTracker.tick();
-            
-            while(openGuiKey.wasPressed()) {
+            while (openGuiKey.wasPressed()) {
                 if (client.player != null) {
                     client.setScreen(new GearReaderScreen());
                 }
