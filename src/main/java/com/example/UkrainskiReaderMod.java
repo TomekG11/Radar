a13dcc692e9b03a06f471cbf672f52b05b8f6054
TomekG11 <tomekgregori21@gmail.com>
2026-03-27 17:15:58 +0100

    Update UkrainskiReaderMod.java

--- src/main/java/com/example/UkrainskiReaderMod.java
+++ src/main/java/com/example/UkrainskiReaderMod.java
@@ -3,29 +3,27 @@ package com.example;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.class_304;
-import net.minecraft.class_3675.class_307;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 
 public class UkrainskiReaderMod implements ClientModInitializer {
-    public static class_304 openGuiKey;
+    public static KeyBinding openGuiKey;
 
-    @Override
     public void onInitializeClient() {
         BlacklistTracker.init();
-        
-        openGuiKey = KeyBindingHelper.registerKeyBinding(new class_304(
+        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "key.ukrainskireader.open",
-            class_307.field_1668,
+            InputUtil.Type.KEYSYM,
             71, // G key
             "category.ukrainskireader"
         ));
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+        
+        ClientTickEvents.END_CLIENT_TICK.register((client) -> {
             PlayerTracker.tick();
             
-            while (openGuiKey.method_1436()) {
-                if (client.field_1724 != null) {
-                    client.method_1507(new GearReaderScreen());
+            while(openGuiKey.wasPressed()) {
+                if (client.player != null) {
+                    client.setScreen(new GearReaderScreen());
                 }
             }
         });
